7d098763177ae4cf8e54a9eaeb5284a89a63da70
Glenlivet Zhang <glenlivet.zhang@pwc.com>
2020-01-13 10:39:33 +0800

    segment before ner.

--- contract.py
+++ contract.py
@@ -5,10 +5,13 @@ jiagu.load_userdict('dict/user.dict')
 # load file
 contract_file = open("contract.txt", "r")
 contract_contents = contract_file.read()
-ner = jiagu.ner(contract_contents)
-content_arr = list(contract_contents)
+segs = jiagu.seg(contract_contents)
+ner = jiagu.ner(segs)
+print(segs)
+print(ner)
+content_arr = list(segs)
 for i in range(len(content_arr)):
     if (content_arr[i] != '\n'):
-        print(content_arr[i] + ' ' + ner[i])
+        print('index: ' + str(i) + ':' + content_arr[i] + ' ' + ner[i])
     else:
         print()
